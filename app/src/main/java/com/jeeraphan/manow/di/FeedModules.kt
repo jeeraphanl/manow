5d21610267f295fa07075549804b0868645e65a5
Jeeraphan Lairat <m@appsynth.net>
2019-05-26 13:23:56 +0700

    resolve presenter with koin

--- app/src/main/java/com/jeeraphan/manow/di/FeedModules.kt
+++ app/src/main/java/com/jeeraphan/manow/di/FeedModules.kt
@@ -4,6 +4,8 @@ import com.jeeraphan.manow.data.repository.NewsRepository
 import com.jeeraphan.manow.data.repository.NewsRepositoryImpl
 import com.jeeraphan.manow.domain.GetFeedUseCase
 import com.jeeraphan.manow.domain.GetFeedUseCaseImpl
+import com.jeeraphan.manow.presentation.mvp.FeedContract
+import com.jeeraphan.manow.presentation.mvp.FeedPresenter
 import com.jeeraphan.manow.presentation.mvvm.FeedViewModel
 import org.koin.android.viewmodel.ext.koin.viewModel
 import org.koin.dsl.module.module
@@ -13,6 +15,8 @@ val feedModule = module {
     factory<NewsRepository> { NewsRepositoryImpl(get()) }
 
     factory<GetFeedUseCase> { GetFeedUseCaseImpl(get()) }
+
+    factory<FeedContract.Presenter> { (view: FeedContract.View) -> FeedPresenter(view, get()) }
 
     viewModel { FeedViewModel(get()) }
 }
--- app/src/main/java/com/jeeraphan/manow/presentation/mvp/FeedActivity.kt
+++ app/src/main/java/com/jeeraphan/manow/presentation/mvp/FeedActivity.kt
@@ -3,31 +3,18 @@ package com.jeeraphan.manow.presentation.mvp
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import com.jeeraphan.manow.R
-import com.jeeraphan.manow.data.datasource.Api
-import com.jeeraphan.manow.data.datasource.OkHttpBuilder
-import com.jeeraphan.manow.data.datasource.RetrofitBuilder
 import com.jeeraphan.manow.data.entity.response.Article
-import com.jeeraphan.manow.data.repository.NewsRepositoryImpl
-import com.jeeraphan.manow.domain.GetFeedUseCaseImpl
 import kotlinx.android.synthetic.main.activity_main.*
-import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
-import retrofit2.converter.gson.GsonConverterFactory
+import org.koin.android.ext.android.inject
+import org.koin.core.parameter.parametersOf
 
 class FeedActivity : AppCompatActivity(), FeedContract.View {
 
-    private lateinit var presenter: FeedContract.Presenter
+    private val presenter: FeedContract.Presenter by inject { parametersOf(this) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val retrofit = RetrofitBuilder(OkHttpBuilder().build(),
-                GsonConverterFactory.create(),
-                RxJava2CallAdapterFactory.create())
-        val api: Api = retrofit.build("https://newsapi.org/v2/")
-        val repository = NewsRepositoryImpl(api)
-        val useCase = GetFeedUseCaseImpl(repository)
-        presenter = FeedPresenter(this, useCase)
 
         presenter.getFeed()
     }
