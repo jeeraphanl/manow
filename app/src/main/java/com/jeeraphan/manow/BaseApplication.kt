11a9c5317fb6908ec3e1907b741b6d2fa57d6b85
jlairat <m@appsynth.net>
2021-01-23 15:55:58 +0700

    update code

--- app/src/main/java/com/jeeraphan/manow/BaseApplication.kt
+++ app/src/main/java/com/jeeraphan/manow/BaseApplication.kt
@@ -1,13 +1,9 @@
 package com.jeeraphan.manow
 
 import android.app.Application
-import com.jeeraphan.manow.di.feedModule
-import com.jeeraphan.manow.di.networkModule
-import org.koin.android.ext.android.startKoin
 
 class BaseApplication : Application() {
     override fun onCreate() {
         super.onCreate()
-        startKoin(this, listOf(networkModule, feedModule))
     }
 }
